495cb9c0a457601123e4ebb0cc54ea592f43c509
Subhranshu Pati <subhranshupati0412@gmail.com>
2023-05-05 11:14:26 +0530

    Login with Google 🔒

--- admin/pages/index.js
+++ admin/pages/index.js
@@ -1,9 +1,22 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+
 export default function Home() {
-  return (
-    <>
-      <div className={'flex flex-col items-center justify-center min-h-screen py-2'}>
-        Test
+  const { data: session } = useSession()
+  if (!session) return (
+    <div className={"bg-black w-screen h-screen flex items-center"}>
+      <div className={"text-center w-full"}>
+        <button onClick={() => signIn('google')} className={"bg-white p-2 px-4 rounded-lg"} >Login with Google</button>
       </div>
-    </>
+    </div>
   )
+
+  return (
+    <div className={"bg-black w-screen h-screen flex items-center"}>
+      <div className={"text-center w-full"}>
+        <h1 className={"text-white text-2xl"}>Welcome {session.user.name}</h1>
+        <button className={"bg-white p-2 px-4 rounded-lg"} onClick={() => signOut()}>Logout</button>
+      </div>
+    </div>
+  )
+
 }

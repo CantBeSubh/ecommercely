50f2bc3312079af396d452b3b0ad29e0a3fea395
Subhranshu Pati <subhranshupati0412@gmail.com>
2023-05-05 21:18:43 +0530

    dashboard

--- admin/pages/index.js
+++ admin/pages/index.js
@@ -1,9 +1,19 @@
 import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
+  const { data: session } = useSession();
+  if (!session) return;
+
   return (
     <Layout>
-      test
+      <div className="flex justify-between">
+        <h1>Hello, {session?.user?.name}!</h1>
+        <div className="flex bg-gray-500 gap-1 rounded-lg overflow-hidden text-white">
+          <img src={session.user.image} alt="user image" className="w-6 h-6" />
+          <span className="px-2">{session.user.name}</span>
+        </div>
+      </div>
     </Layout>
   )
 }

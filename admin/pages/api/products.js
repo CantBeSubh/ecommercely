4b02491a7090b9f7ba131070ad81a8ed2963a3ce
Subhranshu Pati <subhranshupati0412@gmail.com>
2023-05-06 18:59:23 +0530

    table 🥄

--- admin/pages/api/products.js
+++ admin/pages/api/products.js
@@ -5,9 +5,14 @@ export default async function handeler(req, res) {
 
     await mongooseConnect();
     const { method } = req;
+
     if (method === 'POST') {
         const { name, price, description } = req.body;
         const ProductDoc = await Product.create({ name, price, description })
+        res.status(200).json(ProductDoc)
+    }
+    else if (method === 'GET') {
+        const ProductDoc = await Product.find({})
         res.status(200).json(ProductDoc)
     }
 }
--- admin/pages/products/index.js
+++ admin/pages/products/index.js
@@ -1,12 +1,49 @@
 import Layout from "@/components/Layout";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Products() {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        fetch('/api/products').then(res => res.json()).then(data => setProducts(data))
+    }, [])
     return (
         <Layout>
             <Link href="/products/new" className="text-white bg-gray-700 rounded-md py-1 px-2">
                 Add New Product
             </Link>
+            <h1>Products</h1>
+            <hr />
+            <br />
+            <table className="table-auto">
+                <thead>
+                    <tr>
+                        <th className="px-4 py-2">Name</th>
+                        <th className="px-4 py-2">Description</th>
+                        <th className="px-4 py-2">Price</th>
+                        <th className="px-4 py-2">Modify</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map(product => (
+                        <tr key={product._id}>
+                            <td className="border px-4 py-2">{product.name}</td>
+                            <td className="border px-4 py-2">{product.description}</td>
+                            <td className="border px-4 py-2">{product.price}</td>
+                            <td className="border px-4 py-2">
+                                <Link href={`/products/${product._id}`} className="text-white bg-gray-700 rounded-md p-2 inline-flex">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                                    </svg>
+                                </Link>
+                            </td>
+
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
         </Layout>
     )
 }

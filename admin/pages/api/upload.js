5d988c396f8d1dd32371bc1083dfda5fb3747121
Subhranshu Pati <subhranshupati0412@gmail.com>
2023-05-07 12:25:50 +0530

    AWS Upload ☁️

--- admin/pages/api/upload.js
+++ admin/pages/api/upload.js
@@ -1,4 +1,8 @@
 import multiparty from 'multiparty'
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import fs from 'fs'
+import mime from 'mime-types'
+
 
 export default async function handler(req, res) {
     const form = new multiparty.Form()
@@ -9,8 +13,29 @@ export default async function handler(req, res) {
             resolve({ fields, files })
         })
     })
-
-    res.json({ fields, files })
+    const client = new S3Client({
+        region: process.env.AWS_REGION,
+        credentials: {
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        },
+    })
+    const links = []
+    for (const file of files.file) {
+        const ext = file.originalFilename.split('.').pop()
+        const fileName = `${file.originalFilename}-${Date.now()}.${ext}`
+        await client.send(
+            new PutObjectCommand({
+                Bucket: process.env.AWS_BUCKET_NAME,
+                Key: fileName,
+                Body: fs.readFileSync(file.path),
+                ACL: 'public-read',
+                ContentType: mime.lookup(file.path),
+            })
+        )
+        links.push(`https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${fileName}`)
+    }
+    res.json({ links })
 
 }
 

d50a3c68d03f49578f468cf86d4450c2eb7d3b48
Subhranshu Pati <subhranshupati0412@gmail.com>
2023-07-08 13:17:42 +0530

    Logout button

--- admin/components/Nav.js
+++ admin/components/Nav.js
@@ -1,3 +1,4 @@
+import { signOut } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
@@ -9,16 +10,16 @@ export default function Nav() {
     const { pathname } = router
 
     return (
-        <aside className="text-white p-4 pr-0">
+        <aside className="text-white py-4 pl-2">
             <Link href={'/'} className="flex gap-1 mb-4 mr-4">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-8 h-8">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                 </svg>
-                <span>
-                    Ecommercely Admin
+                <span className="text-xl font-bold text-center">
+                    Ecommercely
                 </span>
             </Link>
-            <nav className="flex flex-col gap-2">
+            <nav className="flex flex-col gap-2 ">
                 <Link href={"/"} className={pathname == "/" ? active : inactive}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
@@ -53,6 +54,12 @@ export default function Nav() {
                     </svg>
                     Settings
                 </Link>
+                <button className={"flex gap-1 p-1 border-2 border-black bg-white text-black rounded-lg"} onClick={() => signOut()}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+                    </svg>
+                    Logout
+                </button>
             </nav>
         </aside>
     )
